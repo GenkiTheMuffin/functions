9bd71545764d74de3f844fef2296e9f0f36bc124
Genki <owlysk@gmail.com>
2023-11-05 16:02:36 +0100

    .

--- lcm/main.py
+++ lcm/main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 
+
+def gcd(a: int, b: int):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
+
 def lcm(a: int, b: int):
-    product = a * b
-    while b != 0:
-        a, b = b, a % b
-    return product // a
+    result = abs(a * b) // gcd(a, b)
+    return result
 
 
 print(lcm(12, 10))
