01650af795f003a8d9c0657976ede8c359b382e3
Patrick Larsen <muffin@Patricks-MacBook-Pro.local>
2023-11-10 09:09:58 +0100

    .

--- gcd/main.py
+++ gcd/main.py
@@ -1,13 +1,1 @@
-#!/usr/bin/env python3
-
-
-def gcd(a: int, b: int):
-    while a != b:
-        if a > b:
-            a = a - b
-        else:
-            b = b - a
-    return a
-
-
-print(gcd(128, 28))
+#!/usr/bin/env python3def gcd(a: int, b: int):    while a != b:        if a > b:            a = a - b        else:            b = b - a    return aprint(gcd(128, 28))
